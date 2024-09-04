21cb8d21565976b22efecf5ad2b9058f2e87ca65
Elerrandro <elerrandrosousa@gmail.com>
2024-09-04 12:58:55 -0300

    Mudanças

---
+++ dados.js
@@ -0,0 +1,28 @@
+let jogos = [
+    {
+        nome: "Minecraft",
+        descricao: "Minecraft é um jogo de construção e aventura em um mundo aberto, criado pela Mojang Studios e lançado oficialmente em 2011. No jogo, os jogadores exploram um mundo tridimensional gerado de forma procedural, composto por blocos, que representam diferentes materiais como terra, pedra, madeira e água. Os jogadores podem minerar esses blocos e usá-los para criar estruturas, ferramentas e outros itens.",
+        site: "https://www.minecraft.net/pt-br"
+    },
+    {
+        nome: "Roblox",
+        descricao: "O Roblox é uma plataforma online de jogos criada em 2006 que permite que os usuários criem seus próprios mundos virtuais e jogos. É como um Lego digital, onde você pode construir tudo o que imaginar, desde simples casas até cidades inteiras e jogos complexos.",
+        site: "https://www.roblox.com/pt"
+    },
+    {
+        nome: "Rocket League",
+        descricao: "O Rocket League foi lançado em 2015 e rapidamente se tornou um sucesso, sendo aclamado pela crítica e pelos jogadores. A combinação única de futebol e carros, junto com a jogabilidade divertida e acessível, contribuíram para o sucesso do jogo.",
+        site: "https://www.rocketleague.com/pt-br"
+
+    },
+    {
+        nome: "Genshin Impact",
+        descricao: "Genshin Impact é um jogo de ação e RPG desenvolvido pela miHoYo, lançado em setembro de 2020. O jogo se passa em um vasto mundo aberto chamado Teyvat, onde os jogadores exploram diversas regiões inspiradas em diferentes culturas e mitologias. O jogo se destaca por seu estilo visual vibrante, combate baseado em elementos, e a mecânica de gacha para adquirir novos personagens e armas.",
+        site: "https://genshin.hoyoverse.com/pc-launcher/?utm_source=GS-BR-PC-X-search-keywords-classic-20230303-lowbid&mhy_trace_channel=ga_channel&new=1&gad_source=1&gclid=Cj0KCQjwiuC2BhDSARIsALOVfBKKdoizdTWywXZ6wO-QSH149Vjs_ZvZ6CVOWBNDbZo7O96Tqks0jmwaAk3tEALw_wcB#/"
+    },
+    {
+        nome: "Grand Theft Auto V",
+        descricao: "Um jogo de mundo aberto de ação e aventura desenvolvido pela Rockstar Games. O jogador assume o controle de três protagonistas enquanto navegam pelo submundo criminoso de Los Santos.",
+        site: "https://www.rockstargames.com/br/gta-v"
+    }
+];

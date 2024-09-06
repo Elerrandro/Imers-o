59aa6148017e5f15550591d2847cda2b4e660e04
Elerrandro <elerrandrosousa@gmail.com>
2024-09-06 14:31:16 -0300

    upgrade

--- dados.js
+++ dados.js
@@ -34,7 +34,13 @@ let jogos = [
         nome: "Grand Theft Auto: San Andreas",
         descricao: "lançado em 26 de outubro de 2004 para PlayStation 2. O jogo te coloca no papel de Carl 'CJ' Johnson, um ex-gangster que retorna para sua cidade natal, Los Santos, inspirada em Los Angeles. A trama envolve gangues rivais, corrupção policial, e a jornada de CJ para limpar o nome de sua família e reconquistar seu território.",
         abreviacao: "GTA SA, GTA",
-        site: "https://www.rockstargames.com/br/games/sanandreas",
-    }   
+        site: "https://www.rockstargames.com/br/games/sanandreas"
+    },
+    {
+        nome: "Valorant",
+        descricao: "É um jogo de tiro tático em primeira pessoa (FPS) desenvolvido pela Riot Games, a mesma empresa responsável pelo League of Legends. O jogo se destaca por suas mecânicas de tiro precisas, personagens únicos com habilidades especiais e um sistema de rounds baseado em ataques e defesas. Valorant foi lançado oficialmente em 2 de junho de 2020 para Microsoft Windows.",
+        abreviacao: "VAL, VLR, VT",
+        site: "https://playvalorant.com/pt-br/?gad_source=1&gclid=Cj0KCQjw0Oq2BhCCARIsAA5hubWpmN4oECBEbPM6QoTsfU6vRlGmg6yN0vCPQc_KMQKVckn5XKE7If8aAktdEALw_wcB&gclsrc=aw.ds"
+    }
 
 ];
